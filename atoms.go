e6d53a5b2bd8472c607ac61670ce6c7f687032a3
zoid <zoid@ecwid.com>
2021-10-04 11:42:49 +0400

    refactoring

--- atoms.go
+++ atoms.go
@@ -13,7 +13,7 @@ const (
 	functionClearText            = `function(){("INPUT"===this.nodeName||"TEXTAREA"===this.nodeName)?this.value="":this.innerText=""}`
 	functionGetText              = `function(){switch(this.tagName){case"INPUT":case"TEXTAREA":return this.value;case"SELECT":return Array.from(this.selectedOptions).map(b=>b.innerText).join();default:return this.innerText||this.textContent;}}`
 	functionDispatchEvents       = `function(l){for(const e of l)this.dispatchEvent(new Event(e,{'bubbles':!0}))}`
-	functionPreventMissClick     = `function(){tt=this,z=function(b){for(var c=b;c;c=c.parentNode)if(c==tt)return!0;return!1},i=function(b){if (z(b.target)) {_control_click("1")} else {b.stopPropagation();b.preventDefault();_control_click(b.target.outerHTML||"0")}},document.addEventListener("click",i,{capture:!0,once:!0})}`
+	functionPreventMissClick     = `function(){tt=this,z=function(b){for(var c=b;c;c=c.parentNode)if(c==tt)return!0;return!1},i=function(b){if (z(b.target)) {_control_click("1")} else {b.stopPropagation();b.preventDefault();_control_click((b.target.outerHTML||"").substr(0,256))}},document.addEventListener("click",i,{capture:!0,once:!0})}`
 	functionSetAttr              = `function(a,v){this.setAttribute(a,v)}`
 	functionGetAttr              = `function(a){return this.getAttribute(a)}`
 	functionCheckbox             = `function(v){this.checked=v}`
